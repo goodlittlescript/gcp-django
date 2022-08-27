bba5947347e13af6b15083d4e20dafa82ca48c38
Simon Chiang <simon.a.chiang@gmail.com>
2022-08-27 12:40:06 -0600

    Basic django cleanup

--- src/gcp_django/settings.py
+++ src/gcp_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+at*-v_$d%s9()g&mi0chpi!nfxslq#duh=ngtxd^7kth1)tk'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = (os.environ.get('DJANGO_DEBUG', 'false') == 'true')
 
 ALLOWED_HOSTS = []
 
@@ -74,10 +72,10 @@ WSGI_APPLICATION = 'gcp_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
