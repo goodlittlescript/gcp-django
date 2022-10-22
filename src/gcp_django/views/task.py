a288fafac73142596e8d816f39620c1f49c50b9f
Simon Chiang <simon.a.chiang@gmail.com>
2022-10-22 12:13:18 -0600

    Fix tasks

--- src/gcp_django/views/task.py
+++ src/gcp_django/views/task.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from google.cloud import tasks_v2
 import os
+import json
 
 import logging
 logger = logging.getLogger(__name__)
@@ -22,15 +23,18 @@ def enqueue(request):
 
     client = tasks_v2.CloudTasksClient()
     task = {
-        'http_method': 'POST',
-        'url': f'https://${audience}/schedule/run',
-        'headers': {
-            'traceparent': request.headers.get('traceparent', ''),
-        },
-        'oidc_token': {
-            'service_account_email': service_account_email,
-            'audience': audience,
+        'http_request': {
+            'http_method': tasks_v2.HttpMethod.GET,
+            'url': f'https://{audience}/schedule/run',
+            'headers': {
+                'traceparent': request.headers.get('traceparent', ''),
+            },
+            'oidc_token': {
+                'service_account_email': service_account_email,
+                'audience': audience,
+            },
         },
     }
-    response = client.create_task(request={"parent": parent, "task": task})
-    return HttpResponse(response)
+    response = client.create_task(request={"parent": queue, "task": task})
+    return HttpResponse(
+        f'Created: {response.create_time} ({response.http_request.url})')
